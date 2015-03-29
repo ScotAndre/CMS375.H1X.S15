addc494566b508ce196beb0750aac6c6dcee69f7
Scot Andre <sandre@rollins.edu>
2015-03-29 16:30:52 -0400

    Add CourseSectionsGUI.java file
    Added a new class for the Course Selections GUI

---
+++ Wk_10.Homework_3/src/CourseSectionsGUI.java
@@ -0,0 +1,12 @@
+/*
+ *  CourseSectionsGUI.java
+ *  
+ */
+import java.awt.Container;
+
+import javax.swing.JFrame;
+
+public class CourseSectionsGUI extends JFrame {
+	private Container contentPane;
+	
+}
