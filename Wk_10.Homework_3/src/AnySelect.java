f536a4ab50bb8390d67d392c14292a128e69aa0c
Scot Andre <sandre@rollins.edu>
2015-03-29 09:56:59 -0400

    Added the homework instructions and list of deliverables.

--- Wk_10.Homework_3/src/AnySelect.java
+++ Wk_10.Homework_3/src/AnySelect.java
@@ -1,5 +1,45 @@
-/**
- * Created by Scot on 3/29/2015.
+/*
+ *  AnySelect.java
+ *
+ *  Scot Andre
+ *  CMS375 Spring 2015
+ *  Professor Anderson
+ *  31 March 2015
+ *
+ *  Assignment instructions:
+ *  Write a Java application that uses JDBC to display sections and semester
+ *  for a course.
+ *  First your program should prompt the user for the user name and password.
+ *  Then your program should read the course names from the database, display
+ *  all the course names, and prompt the user for which course they want the
+ *  information.
+ *
+ *  You can build a GUI for this program if you want. In that case, the user
+ *  would select the course name from a JComboBox.
+ *
+ *  Example output if the user chooses "Intro to C++":
+ *
+ *     Course Name             Section       Semester
+ *     Intro to C++                  2       Fall
+ *                                   1       Fall
+ *
+ *  Note that the course name is outputed only once, even if the course has
+ *  multiple sections.
+ *
+ *  Deliverables:
+ *    Your Java source code (zipped) and a report that answers these questions:
+ *
+ *    1. Explain the roles that each of these components play in JDBC:
+ *       a. JDBC Driver
+ *       b. Connection
+ *       c. Session
+ *       d. Statement
+ *       e. ResultSet
+ *       f. ResultSet MetaData
+ *
+ *    2. When should you use the 'executeUpdate' method versus the
+ *       'executeQuery' method? In other words, what is the difference between
+ *       these two statements?
  */
 public class AnySelect {
 }
